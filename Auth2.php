75543608de724f4501d00df191887fba4f86be10
Apc204 <cresswell.apc@gmail.com>
2012-11-05 15:42:45 +0000

    Needs unique GET variable names.

--- Auth2.php
+++ Auth2.php
@@ -5,53 +5,60 @@ require_once '/HTTP/OAuth/Consumer.php';
 require_once 'HTTP.php';
 
 	session_start();
-	if ($_SESSION['set'] == 'no')
-	{
-		$consumer = new HTTP_OAuth_Consumer('d8e4a5bdaedbd31f6f322437d0a38c1805060529f','161a05857f0c8293e067644f01f0d12d', $_SESSION['token'], $_SESSION['token_secret']);
+	echo $_SESSION['set'];
+	echo '<br>';
 
-		$consumer->getAccessToken('http://api.mendeley.com/oauth/access_token/',$_GET['oauth_verifier'],array(),'GET');
+	echo '*******************************';
+	$consumer = new HTTP_OAuth_Consumer('d8e4a5bdaedbd31f6f322437d0a38c1805060529f','161a05857f0c8293e067644f01f0d12d', $_SESSION['token'], $_SESSION['token_secret']);
+	$consumer->getAccessToken('http://api.mendeley.com/oauth/access_token/',$_GET['oauth_verifier'],array(),'GET');
 			
-		$_SESSION['token'] = $consumer->getToken();
-		$_SESSION['token_secret'] = $consumer->getTokenSecret();
-		$_SESSION['set'] = 'yes';
+	$_SESSION['token'] = $consumer->getToken();
+	$_SESSION['token_secret'] = $consumer->getTokenSecret();
 
 		
-		$response = $consumer->sendRequest('http://api.mendeley.com/oapi/library/',array(),'GET');
+	$response = $consumer->sendRequest('http://api.mendeley.com/oapi/library/',array(),'GET');
 
-		$docs = $response->getDataFromBody();
+	$docs = $response->getDataFromBody();
 		
-		$_SESSION['docs'] = array();
+	$_SESSION['docs'] = array();
 
-		foreach ($docs as $elem)
-		{
-			$_SESSION['docs'] = explode(",",array_keys($elem)[0]);
-			//Print_r($_SESSION['docs']);
-		}
+	foreach ($docs as $elem)
+	{
+		$_SESSION['docs'] = explode(",",array_keys($elem)[0]);
+		//Print_r($_SESSION['docs']);
 	}
+
+	
+	echo '<form action="mendeleyImport.php" method="GET">';
 	
 	foreach ($_SESSION['docs'] as $ID)
 	{
 		echo '<br>';
 		$url = "http://api.mendeley.com/oapi/library/documents/$ID/";
 		$url2 = str_replace("\"","",$url);
-		echo $url;
-		echo '<br>';
-		$response = $consumer->sendRequest($url2,array(),'GET');
+		$count = 0;
 
+		$response = $consumer->sendRequest($url2,array(),'GET');
 		$details = $response->getResponse();
 		$string = $details->getBody();
-		//echo 'STRING STARTS HERE: '.$string.' AND ENDS HERE!';
-		$jsonArray = json_decode($string);
-		print_r($jsonArray);
+		$_SESSION['jsonArray'] = json_decode($string, TRUE);
+		
+		echo htmlentities($_SESSION['jsonArray']['title'].'<input type="checkbox" name="choices" value="'.$ID.'">');
+		$choice = 'choices'.$count;
+		$choice = str_replace("\"","",$choice);
+		$temp = $_SESSION['jsonArray']['title'].'<input type="checkbox" name='.$choice.' value='.$ID.'>';
+		$count++;
+		echo $temp;
+		echo '<br>*******************************<br>';
 		//$string = BetweenStr($string, '[body:protected] => ','[bodyEncoded:protected]');
 		//echo '<br> DOCUMENT DETAILS <BR>';
 		//Print_r($string);
-	//echo $ID.'<input type="checkbox" name="ID" value=\"'.$ID.'\"><br><br><br>';
+		//echo $ID.'<input type="checkbox" name="ID" value=\"'.$ID.'\"><br><br><br>';
 	}
+	echo '<input type="submit" value="Submit"><br></form>';
 
-	//HTTP::redirect('mendeleyImport.php');
-
-	function BetweenStr($InputString, $StartStr, $EndStr=0, $StartLoc=0) {
+	function BetweenStr($InputString, $StartStr, $EndStr=0, $StartLoc=0) 
+	{
 		if (($StartLoc = strpos($InputString, $StartStr, $StartLoc)) === false) { return; }
 		$StartLoc += strlen($StartStr);
 		if (!$EndStr) { $EndStr = $StartStr; }
@@ -60,5 +67,3 @@ require_once 'HTTP.php';
 	}
 	
 ?>
-
-Authenticated.
--- mendeleyImport.php
+++ mendeleyImport.php
@@ -2,23 +2,7 @@
 
 session_start();
 
-//echo '<form action="mendeleyImport.php" method="POST>';
-//Print_r($_SESSION['docs']);
-echo gettype($_SESSION['docs']);
-
-foreach ($_SESSION['docs'] as $ID)
-{
-	$
-	echo $ID.'<input type="checkbox" name="ID" value=\"'.$ID.'\"><br><br><br>';
-}
-
-
-
-
-
-
-
-
+echo $_GET['choices'];
 
 
 
